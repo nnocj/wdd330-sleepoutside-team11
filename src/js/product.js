1590d536abbd8ed377bc439ffefdbb3ee6133d87
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2026-02-10 13:29:07 +0000

    completed week 3 stretch activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,11 @@
-import { getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
+
+loadHeaderFooter();
 
 // this is to test if the getParam function can use the query string to display the correct product details on the product page.
 const product = new ProductDetails(productId, dataSource);
