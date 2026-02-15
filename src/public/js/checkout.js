dcbbd1c9559509ccd302bfb01dd1914df86a7c90
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2026-02-15 05:57:01 +0000

    adding checkout page

---
+++ src/public/js/checkout.js
@@ -0,0 +1,18 @@
+/**This file is to be placed in the checkout/index.html page
+ * 1. its to ensure that alll calculation functions needed to render order summary is obtained
+ * 2 Send data to server.
+ */
+
+import { loadHeaderFooter } from "./utils.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs";
+
+
+loadHeaderFooter();
+
+//listing the parameters to run the checkout process class.
+const key = "so-cart"
+const outputElement = document.querySelector("#order-details");
+
+//I'm ensuring product summary details can be obtained.
+const checkoutProcess = new CheckoutProcess(key, outputElement);
+checkoutProcess.init();
